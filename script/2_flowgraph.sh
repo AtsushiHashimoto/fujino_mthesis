3252ea7e6c59b3615f251cf45b807d81f320a505
Atsushi <ahasimoto@mm.media.kyoto-u.ac.jp>
2017-06-16 07:35:56 +0900

    uncomment necessary parts

--- script/2_flowgraph.sh
+++ script/2_flowgraph.sh
@@ -9,35 +9,35 @@ MYSQL_DB=cookpad_data
 echo "Msg: Make sure that mysql is alive!"
 # 各レシピIDの画像のディレクトリと枚数を保存　
 mkdir -p ../exp/recipes
-#python ../tools/flowgraph/obtain_recipe_location_steps.py ../external/IMG/step ../exp/ 
+python ../tools/flowgraph/obtain_recipe_location_steps.py ../external/IMG/step ../exp/ 
 
 # 陰性訓練データ用に「今日の料理」カテゴリのレシピIDを取得
-#python ../tools/flowgraph/obtain_recipes_from_a_category.py \
-#	-output_dir ../exp/recipes \
-#	-seed 0 \
-#	-usr ${MYSQL_USR} \
-#	-socket ${MYSQL_SOCKET} \
-#	-host ${MYSQL_HOST} \
-#	-db ${MYSQL_DB}\
-#	-password ${MYSQL_PASSWORD} \
-#	-recipe_img_dir_path ../exp/recipe_location_steps.json \
-#	-category_name 今日の料理
-##	-rcp_loc_steps_path ../exp/recipe_location_steps.json \
-
-## 陽性訓練データ用レシピIDを取得
-#python ../tools/flowgraph/obtain_recipes_from_ingredients.py \
-#	-ingredients ${FOODS} \
-#	-synonym_path ../external/ontology/synonym.tsv \
+python ../tools/flowgraph/obtain_recipes_from_a_category.py \
+	-output_dir ../exp/recipes \
+	-seed 0 \
+	-usr ${MYSQL_USR} \
+	-socket ${MYSQL_SOCKET} \
+	-host ${MYSQL_HOST} \
+	-db ${MYSQL_DB}\
+	-password ${MYSQL_PASSWORD} \
+	-recipe_img_dir_path ../exp/recipe_location_steps.json \
+	-category_name 今日の料理
 #	-rcp_loc_steps_path ../exp/recipe_location_steps.json \
-#	-output_dir ../exp/recipes \
-#	-seed 0 \
-#	-usr ${MYSQL_USR} \
-#	-socket ${MYSQL_SOCKET} \
-#	-host ${MYSQL_HOST} \
-#	-db ${MYSQL_DB} \
-#	-password ${MYSQL_PASSWORD} \
-#
-# max spanning tree(フローグラフ)の推定とフローグラフを辿ってラベルを推定
+
+# 陽性訓練データ用レシピIDを取得
+python ../tools/flowgraph/obtain_recipes_from_ingredients.py \
+	-ingredients ${FOODS} \
+	-synonym_path ../external/ontology/synonym.tsv \
+	-rcp_loc_steps_path ../exp/recipe_location_steps.json \
+	-output_dir ../exp/recipes \
+	-seed 0 \
+	-usr ${MYSQL_USR} \
+	-socket ${MYSQL_SOCKET} \
+	-host ${MYSQL_HOST} \
+	-db ${MYSQL_DB} \
+	-password ${MYSQL_PASSWORD} \
+
+max spanning tree(フローグラフ)の推定とフローグラフを辿ってラベルを推定
 mkdir -p ../exp/flowgraph/flow/
 mkdir -p ../exp/flowgraph/label/
 python ../tools/flowgraph/gen_msts_and_labels.py \
